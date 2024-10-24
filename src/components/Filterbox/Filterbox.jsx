ecce18ef64ef8aaeeb823a1bf20482b2e45f24f7
Thilakh <thilakh2004@gmail.com>
2024-10-24 06:16:01 +0530

    final

--- src/components/Filterbox/Filterbox.jsx
+++ src/components/Filterbox/Filterbox.jsx
@@ -1,6 +1,6 @@
 import { useState,useEffect } from "react"
 import { generateDataOptions,months,years } from "../../utils/DataRender"
-import "./FilterBox.css"
+import "./Filterbox.css"
 const FilterBox = ({getMonthYear})=>{
     const [selectedMonth,setSelectedMonth]=useState("January");
     const [selectedYear,setSelectedYear]=useState(2023);
